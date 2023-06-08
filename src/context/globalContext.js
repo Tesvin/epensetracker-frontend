5d57c7a95bbb7bd1e43ab30369cedf8ab054829d
stephen azosike <stephenazosike@gmail.com>
2023-06-08 11:33:43 +0100

    frontend

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from "react"
 import axios from 'axios'
 
-const BASE_URL = "https://expensetracker-backend-production-3d0f.up.railway.app/"
+const BASE_URL = "expensetracker-backend-production-3d0f.up.railway.app/"
+// const BASE_URL = "https://expensetracker-backend-production-3d0f.up.railway.app/"
 // const BASE_URL = "http://localhost:3000/api/v1/";
 
 
